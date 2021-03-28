6826c9ca4d3ab71c2f5b2cfb8fb93e497783b746
jdxyw <jdxyw2004@gmail.com>
2021-03-28 21:54:19 +0800

    update

--- postreq2.py
+++ postreq2.py
@@ -8,13 +8,15 @@ import bs4
 ws = [1,2,3,4,5,6,7]
 
 for w in ws:
-    r = requests.post("http://www.5iarraildental.com/index.php?s=/Home/Index/getClinicListByArea.html", data={"w1": w})
-    j = json.loads(r.content)
-    h = bs4.BeautifulSoup(j["html"], features="html.parser")
-    print(h)
-    cityhtml = ET.fromstring("<xml>"+str(h.contents[0])+"</xml>")
-    for e in cityhtml.findall("li/div/div/div"):
-        ss = e.findall("span")
-        print("{}\t{}\t{}\t{}".format(e.find("a").text, ss[0].text, ss[1].text,ss[2].text))
-
-    break
+    for pg in range(1,4):
+        r = requests.post("http://www.5iarraildental.com/index.php?s=/Home/Index/getClinicListByArea.html", data={"w1": w, "page":pg})
+        j = json.loads(r.content)
+        h = bs4.BeautifulSoup(j["html"], features="html.parser")
+        s = str(h)
+        cityhtml = ET.fromstring("<xml>"+str(s)+"</xml>")
+        # print(cityhtml.getchildren())
+        for e in cityhtml.findall("li/div/div"):
+            d = e.getchildren()
+            p = d[1].find("a").find("p")
+            ss = d[1].findall("p/span")
+            print("{}\t{}\t{}\t{}".format(p.text, ss[0].text, ss[1].text,ss[2].text))
